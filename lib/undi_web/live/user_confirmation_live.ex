971418f93d8acc76c2458b4ac40eace8bdf641dc
dev-rroslan <dev.rroslan@gmail.com>
2023-03-19 20:34:58 +0800

    from dev to dev10 and delete register in confirmation

--- lib/undi_web/live/user_confirmation_live.ex
+++ lib/undi_web/live/user_confirmation_live.ex
@@ -15,7 +15,6 @@ defmodule UndiWeb.UserConfirmationLive do
     </.simple_form>
 
     <p class="text-center">
-      <.link href={~p"/users/register"}>Register</.link>
       |
       <.link href={~p"/users/log_in"}>Log in</.link>
     </p>
